59b9917ee403ca434a4482aecc17879dcc49f296
Mateusz Krzeszowiak <mateusz.krzeszowiak@gmail.com>
2015-02-28 02:23:51 +0100

    Fix autoload for PHP 5.3

--- src/autoload.php
+++ src/autoload.php
@@ -6,7 +6,7 @@ spl_autoload_register(function ($classname) {
     $classname = ltrim($classname, "\\");
     preg_match('/^(.+)?([^\\\\]+)$/U', $classname, $match);
     $classname = str_replace("\\", "/", $match[1])
-        . str_replace(["\\", "_"], "/", $match[2])
+        . str_replace(array("\\", "_"), "/", $match[2])
         . ".php";
     include_once $classname;
 });
