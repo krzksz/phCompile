aea7ac431269e978f486b1294849169ee7fc54da
Mateusz Krzeszowiak <mateusz.krzeszowiak@gmail.com>
2015-03-06 16:58:21 +0100

    Another Travis fix.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,6 @@ define('SRC_PATH', dirname(__FILE__).'/../src/');
 define('TEST_PATH', dirname(__FILE__).'/');
 
 require SRC_PATH . 'autoload.php';
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
